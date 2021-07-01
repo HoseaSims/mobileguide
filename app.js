7d29d8e3fd7424202754609a5a0be6069fb9ad11
Hosea <hoseasims21@gmail.com>
2021-07-01 12:01:48 -0600

    added steps four and five, changed styling on buttons

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const dss = document.querySelector('.delivery-status')
 const stepTwoFT = document.querySelector('.step-two-flash')
 const stepTwoMT = document.querySelector('.step-two-url')
 const stepThree = document.querySelector('.step-three')
+const stepFour = document.querySelector('.step-four')
+const StepFive = document.querySelector('.step-five')
 
 const main = document.querySelector('main')
 
@@ -14,6 +16,7 @@ main.addEventListener('click', (e) =>{
        console.log('yes');
        stepTwoFT.classList.toggle('hidden')
        stepTwoMT.classList.add('hidden')
+       
     }
 
     if(e.target.id === 'mtb'){
@@ -25,4 +28,10 @@ main.addEventListener('click', (e) =>{
      if(e.target.id === 'fulfilled'){
          stepThree.classList.toggle('hidden')
      }
+
+     if(e.target.id === 'exmt'){
+         stepFour.classList.toggle('hidden');
+     }
+
+     
 })
