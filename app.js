5835c9e8ca6e97390b66864e55882302d1ed05e0
Hosea <hoseasims21@gmail.com>
2021-06-30 20:56:41 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const ftb = document.querySelector('#ftb')
+const mtb = document.querySelector('.mtb')
+const dss = document.querySelector('.delivery-status')
+const stepTwoFT = document.querySelector('.step-two-flash')
+const stepTwoMT = document.querySelector('.step-two-url')
+const stepThree = document.querySelector('.step-three')
+
+const main = document.querySelector('main')
+
+main.addEventListener('click', (e) =>{
+    
+
+    if(e.target.id === 'ftb'){
+       console.log('yes');
+       stepTwoFT.classList.toggle('hidden')
+       stepTwoMT.classList.add('hidden')
+    }
+
+    if(e.target.id === 'mtb'){
+        console.log('yes');
+        stepTwoMT.classList.toggle('hidden')
+        stepTwoFT.classList.add('hidden')
+     }
+
+     if(e.target.id === 'fulfilled'){
+         stepThree.classList.toggle('hidden')
+     }
+})
